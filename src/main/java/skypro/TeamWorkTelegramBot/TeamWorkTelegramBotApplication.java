06282d32d1c4f0c5d44f0e45dac785f497098470
grigoriirarog <rarog.grigoriii@gmail.com>
2023-09-03 12:34:17 +0200

    Initial commit

---
+++ src/main/java/skypro/TeamWorkTelegramBot/TeamWorkTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package skypro.TeamWorkTelegramBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamWorkTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamWorkTelegramBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/skypro/TeamWorkTelegramBot/TeamWorkTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package skypro.TeamWorkTelegramBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamWorkTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
