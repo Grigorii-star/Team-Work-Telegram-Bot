2ff8fdae8ddb23c3bf6b7c8dbaa2a182b0590e48
Alexandr.Burmistroff <albook366314@gmail.com>
2023-09-04 09:00:17 +0200

    В properties добавлен путь к liquibase; Создан файл .yml(указан путь к файлу sql); Создан файл .sql(созданы две таблицы для cat/dog питомников).

---
+++ src/main/resources/liquibase/scripts/telegram_bot.sql
@@ -0,0 +1,24 @@
+-- liquibase formatted sql
+
+-- changeset aburmistrov:1
+CREATE TABLE cat_shelter
+(
+    id            bigserial NOT NULL PRIMARY KEY,
+    name          varchar(255),
+    about_shelter varchar(255),
+    location      varchar(255),
+    security      varchar(255), --контактные данные охраны для оформления пропуска на машину
+    safety_advice varchar(255), --техника безопасности на территории приюта
+    timetable     varchar(255)  --расписание работы приюта
+);
+
+CREATE TABLE dog_shelter
+(
+    id            bigserial NOT NULL PRIMARY KEY,
+    name          varchar(255),
+    about_shelter varchar(255),
+    location      varchar(255),
+    security      varchar(255), --контактные данные охраны для оформления пропуска на машину
+    safety_advice varchar(255), --техника безопасности на территории приюта
+    timetable     varchar(255)  --расписание работы приюта
+)
