f9725ef70934895970f51f407563256edcf10ff2
KrozhDev <roko.mdn@gmail.com>
2023-09-04 08:50:19 +0300

    Создан класс TelegramBotService

---
+++ src/main/java/skypro/TeamWorkTelegramBot/service/TelegramBotService.java
@@ -0,0 +1,37 @@
+package skypro.TeamWorkTelegramBot.service;
+
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.Update;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+
+@Service
+public class TelegramBotService implements UpdatesListener {
+    private final TelegramBot telegramBot;
+
+    @Autowired
+    public TelegramBotService(TelegramBot telegramBot) {
+        this.telegramBot = telegramBot;
+    }
+
+    @PostConstruct
+    public void init() {
+        telegramBot.setUpdatesListener(this);
+    }
+
+
+    @Override
+    public int process(List<Update> updates) {
+        updates.forEach(update -> {
+
+
+
+        });
+        return UpdatesListener.CONFIRMED_UPDATES_ALL;
+    }
+}
