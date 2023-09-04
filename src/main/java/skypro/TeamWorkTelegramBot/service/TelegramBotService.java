a118f2ffad4002376052d66c4970afb0aca41d1d
KrozhDev <roko.mdn@gmail.com>
2023-09-04 10:02:14 +0300

    Добавил информацию о питомнике

--- src/main/java/skypro/TeamWorkTelegramBot/service/TelegramBotService.java
+++ src/main/java/skypro/TeamWorkTelegramBot/service/TelegramBotService.java
@@ -9,6 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.List;
 
 
@@ -36,19 +39,44 @@ public class TelegramBotService implements UpdatesListener {
             String userName = update.message().from().firstName();
 
             if (text.equals("/start")){
+                // todo здесь нужно залезть в бд, поискать пользователя, если он уже был, то мы пропускаем приветствие
                 SendMessage message = new SendMessage(chatId, getGreetingText(userName));
                 SendResponse response = telegramBot.execute(message);
+                message = new SendMessage(chatId,getInfo());
+                response = telegramBot.execute(message);
             }
 
+            if (text.equals("1")) {
+
+            }
 
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
 
     private String getGreetingText(String userName) {
-        return "Привет " + userName + "! Я бот, который поможет тебе забрать питомца из нашего приюта в Астане." +
-                "Я отвечу на все вопросы и помогу определиться с выбором. " +
-                "\n\nНапиши 1, если ты хочешь завести собаку. " +
+        return "Привет " + userName + "! Я бот, который поможет тебе забрать питомца из нашего приюта в Астане. " +
+                "Я отвечу на все вопросы и помогу определиться с выбором.";
+
+    }
+
+    private String getInfo() {
+        StringBuilder sb = new StringBuilder();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader("about_shelter.txt"))) {
+            String line = reader.readLine();
+            while (line != null) {
+                sb.append(line);
+                line = reader.readLine();
+            }
+        } catch (IOException e) {
+            System.out.println("Файл не найден");
+    }
+        return sb.toString();
+    }
+
+    private String getChoice() {
+        return  "Напиши 1, если ты хочешь завести собаку. " +
                 "\n\nНапиши 2, если ты хочешь завести кошку. ";
     }
 }
