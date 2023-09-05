b61760d1dbb89ffe2f7c7dbf364315b7effd1245
Alexandr.Burmistroff <albook366314@gmail.com>
2023-09-05 11:10:13 +0200

    Созданы классы всех этапов со всеми названиями методов в них.

---
+++ src/main/java/skypro/TeamWorkTelegramBot/stages/CareStage.java
@@ -0,0 +1,28 @@
+package skypro.TeamWorkTelegramBot.stages;
+
+public class CareStage {
+
+    private String animalPhoto() {
+        return "";
+    }
+
+    private String animalDiet() {
+        return "";
+    }
+
+    private String generalWellBeingAndGettingUsedToANewPlace() {
+        return "";
+    }
+
+    private String changeInBehavior() {
+        return "";
+    }
+
+    private String submitADailyReportForm() {
+        return "";
+    }
+
+    private String callAVolunteer() { //этот метот присутствует на всех этапах
+        return "";
+    }
+}
---
+++ src/main/java/skypro/TeamWorkTelegramBot/stages/StageOfPreparationOfDocuments.java
@@ -0,0 +1,53 @@
+package skypro.TeamWorkTelegramBot.stages;
+
+public class StageOfPreparationOfDocuments {
+
+    private String userGreeting() { //аналогичный метод в ConsultationStage
+        return "Я рад, что ты готов к встрече с новым членом семьи!\n" +
+                "Для продолжения, пожалуйста, ознакомься со следующей документацией.";
+    }
+
+    private String issueRules() {
+        return "";
+    }
+
+    private String issueAListOfDocumentsInOrderToTakeTheAnimal() {
+        return "";
+    }
+
+    private String issueAListOfRecommendationsForTransportation() {
+        return "";
+    }
+
+    private String issueAListOfRecommendationsForHomeImprovementForAPuppyOrKitten() {
+        return "";
+    }
+
+    private String issueAListOfRecommendationsForHomeImprovementForAnAdultAnimal() {
+        return "";
+    }
+
+    private String issueAListOfRecommendationsForHomeImprovementForAnAnimalWithDisabilities() {
+        return "";
+    }
+
+    private String giveCynologistAdviceOnInitialCommunicationWithADog() {
+        return "";
+    }
+
+    private String issueRecommendationsOnProvenCynologistsForFurtherReferralToThem() {
+        return "";
+    }
+
+    private String issueAListOfReasonsForRefusal() {
+        return "";
+    }
+
+    private String acceptAndRecordContactDetails() { //аналогичный метод в ConsultationStage
+        return "";
+    }
+
+    private String callAVolunteer() { //этот метот присутствует на всех этапах
+        return "";
+    }
+}
