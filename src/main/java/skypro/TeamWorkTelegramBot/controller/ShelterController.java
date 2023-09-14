912db2edf051e88f1ac4954dd8e108c13d5371ce
Kinserega <kindratsergej5@gmail.com>
2023-09-15 00:25:57 +0300

    добавил везде Java Doc и swagger

--- src/main/java/skypro/TeamWorkTelegramBot/controller/ShelterController.java
+++ src/main/java/skypro/TeamWorkTelegramBot/controller/ShelterController.java
@@ -1,5 +1,8 @@
 package skypro.TeamWorkTelegramBot.controller;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import skypro.TeamWorkTelegramBot.entity.Shelter;
@@ -18,25 +21,76 @@ public class ShelterController {
     public ShelterController(ShelterService shelterService) {
         this.shelterService = shelterService;
     }
-
+    /**
+     * метод, который добавляет новый приют.
+     * @param shelter объект Shelter, который будет добавлен
+     * @return ResponseEntity с добавленным объектом Shelter
+     */
+    @ApiOperation(value = "Добавить новый приют")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Приют успешно добавлен"),
+            @ApiResponse(code = 400, message = "Предоставлены некорректные данные"),
+            @ApiResponse(code = 500, message = "Ошибка при добавлении приюта")
+    })
     @PostMapping
     public ResponseEntity<Shelter> add(@RequestBody Shelter shelter) {
         return ResponseEntity.ok(shelterService.addShelter(shelter));
     }
+    /**
+     * метод, который находит приют по его идентификатору.
+     * @param id идентификатор приюта
+     * @return ResponseEntity с найденным объектом приюта
+     */
+    @ApiOperation(value = "Найти приют по id")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Приют успешно найден"),
+            @ApiResponse(code = 404, message = "Приют не найден"),
+            @ApiResponse(code = 500, message = "Ошибка при поиске приюта")
+    })
     @GetMapping("{id}")
     public ResponseEntity<Shelter> find(@PathVariable Long id) {
         return ResponseEntity.ok(shelterService.findShelter(id));
-
     }
+    /**
+     * метод, который редактирует информацию о приюте.
+     * @param shelter объект Shelter, содержащий измененную информацию о приюте
+     * @return ResponseEntity с объектом Shelter после редактирования
+     */
+    @ApiOperation(value = "Редактировать приют")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Информация о приюте успешно отредактирована"),
+            @ApiResponse(code = 400, message = "Предоставлены некорректные данные для редактирования"),
+            @ApiResponse(code = 500, message = "Ошибка при редактировании информации о приюте")
+    })
     @PutMapping
     public ResponseEntity<Shelter> edit(@RequestBody Shelter shelter) {
         return ResponseEntity.ok(shelterService.editShelter(shelter));
     }
+    /**
+     * метод, который удаляет приют по его идентификатору.
+     * @param id идентификатор приюта, который будет удален
+     * @return ResponseEntity без тела, указывающий на успешное удаление приюта
+     */
+    @ApiOperation(value = "Удалить приют по ID")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Приют успешно удален"),
+            @ApiResponse(code = 404, message = "Приют не найден"),
+            @ApiResponse(code = 500, message = "Ошибка при удалении приюта")
+    })
     @DeleteMapping("{id}")
     public ResponseEntity<Void> remove(@PathVariable Long id) {
         shelterService.removeShelter(id);
         return ResponseEntity.ok().build();
     }
+    /**
+     * метод, который получает список всех приютов.
+     * @return ResponseEntity с коллекцией объектов Shelter
+     */
+    @ApiOperation(value = "Получить список всех приютов")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Список приютов успешно получен"),
+            @ApiResponse(code = 500, message = "Ошибка при получении списка приютов")
+    })
     @GetMapping
     public ResponseEntity<Collection<Shelter>> getAll() {
         return ResponseEntity.ok(shelterService.getAllShelters());
--- src/main/java/skypro/TeamWorkTelegramBot/controller/VolunteerController.java
+++ src/main/java/skypro/TeamWorkTelegramBot/controller/VolunteerController.java
@@ -1,5 +1,8 @@
 package skypro.TeamWorkTelegramBot.controller;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import skypro.TeamWorkTelegramBot.entity.Volunteer;
@@ -18,25 +21,76 @@ public class VolunteerController {
     public VolunteerController(VolunteerService volunteerService) {
         this.volunteerService = volunteerService;
     }
-
+    /**
+     * метод, который добавляет нового волонтера.
+     * @param volunteer объект Volunteer, который будет добавлен
+     * @return ResponseEntity с добавленным объектом Volunteer
+     */
+    @ApiOperation(value = "Добавить нового волонтера")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Волонтер успешно добавлен"),
+            @ApiResponse(code = 400, message = "Предоставлены некорректные данные"),
+            @ApiResponse(code = 500, message = "Ошибка при добавлении волонтера")
+    })
     @PostMapping
     public ResponseEntity<Volunteer> add(@RequestBody Volunteer volunteer) {
         return ResponseEntity.ok(volunteerService.addVolunteer(volunteer));
     }
+    /**
+     * метод, который находит волонтера по его идентификатору.
+     * @param id идентификатор волонтера
+     * @return ResponseEntity с найденным объектом волонтера
+     */
+    @ApiOperation(value = "Найти волонтера по id")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Волонтер успешно найден"),
+            @ApiResponse(code = 404, message = "Волонтер не найден"),
+            @ApiResponse(code = 500, message = "Ошибка при поиске волонтера")
+    })
     @GetMapping("{id}")
     public ResponseEntity<Volunteer> find(@PathVariable Long id) {
         return ResponseEntity.ok(volunteerService.findVolunteer(id));
-
     }
+    /**
+     * метод, который редактирует информацию о волонтере.
+     * @param volunteer объект Volunteer, содержащий измененную информацию о волонтере
+     * @return ResponseEntity с объектом Volunteer после редактирования
+     */
+    @ApiOperation(value = "Редактировать волонтера")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Информация о волонтере успешно отредактирована"),
+            @ApiResponse(code = 400, message = "Предоставлены некорректные данные для редактирования"),
+            @ApiResponse(code = 500, message = "Ошибка при редактировании информации о волонтере")
+    })
     @PutMapping
     public ResponseEntity<Volunteer> edit(@RequestBody Volunteer volunteer) {
         return ResponseEntity.ok(volunteerService.editVolunteer(volunteer));
     }
+    /**
+     * метод, который удаляет волонтера по его идентификатору.
+     * @param id идентификатор волонтера, который будет удален
+     * @return ResponseEntity без тела, указывающий на успешное удаление волонтера
+     */
+    @ApiOperation(value = "Удалить волонтера по ID")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Волонтер успешно удален"),
+            @ApiResponse(code = 404, message = "Волонтер не найден"),
+            @ApiResponse(code = 500, message = "Ошибка при удалении волонтера")
+    })
     @DeleteMapping("{id}")
     public ResponseEntity<Void> remove(@PathVariable Long id) {
         volunteerService.removeVolunteer(id);
         return ResponseEntity.ok().build();
     }
+    /**
+     * метод, который получает список всех волонтеров.
+     * @return ResponseEntity с коллекцией объектов Volunteer
+     */
+    @ApiOperation(value = "Получить список всех волонтеров")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Список волонтеров успешно получен"),
+            @ApiResponse(code = 500, message = "Ошибка при получении списка волонтеров")
+    })
     @GetMapping
     public ResponseEntity<Collection<Volunteer>> getAll() {
         return ResponseEntity.ok(volunteerService.getAllVolunteers());
