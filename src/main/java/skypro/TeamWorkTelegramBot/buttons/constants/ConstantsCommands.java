d238b4a75b5a68d9ef38227926dc00dbac1db7e5
KrozhDev <roko.mdn@gmail.com>
2023-09-14 20:37:10 +0300

    TelegramBotService заменил на константы, создал класс ConstantsCommands

---
+++ src/main/java/skypro/TeamWorkTelegramBot/buttons/constants/ConstantsCommands.java
@@ -0,0 +1,21 @@
+package skypro.TeamWorkTelegramBot.buttons.constants;
+
+public class ConstantsCommands {
+
+    /**
+     * Константы для ключевых команд бота для поиска в мапе
+     *
+     * @param commandTextFromButtons
+     */
+
+    public static final String START_COMMAND = "start";
+    public static final String MAIN_MENU_COMMAND = "mainMenu";
+    public static final String INFORMATION_COMMAND = "information";
+    public static final String CAT_AND_DOG_INFO_COMMAND = "catAndDogInformation";
+    public static final String SAVE_USER_CONTACTS_COMMAND = "saveUserContacts";
+    public static final String VOLUNTEER_COMMAND = "volunteer";
+    public static final String SAVE_REPORT_COMMAND = "saveReportAboutPet";
+    public static final String GET_ANIMAL_COMMAND = "getAnimalFromTheShelter";
+    public static final String CAT_AND_DOG_GET_ANIMAL_COMMAND = "catAndDogGetAnimalFromTheShelter";
+
+}
