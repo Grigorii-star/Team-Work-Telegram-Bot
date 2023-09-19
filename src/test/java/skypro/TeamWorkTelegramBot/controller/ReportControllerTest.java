873d298863ac0b6b3f3b6d2dac011acab816667c
Alexandr.Burmistroff <albook366314@gmail.com>
2023-09-19 10:57:19 +0200

    черновик test2

--- src/test/java/skypro/TeamWorkTelegramBot/controller/ReportControllerTest.java
+++ src/test/java/skypro/TeamWorkTelegramBot/controller/ReportControllerTest.java
@@ -1,173 +1,173 @@
-package skypro.TeamWorkTelegramBot.controller;
-
-import lombok.SneakyThrows;
-import org.json.JSONObject;
-
-import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-
-import org.springframework.boot.test.mock.mockito.MockBean;
-
-import org.springframework.http.MediaType;
-
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import skypro.TeamWorkTelegramBot.entity.Report;
-import skypro.TeamWorkTelegramBot.repository.ReportsRepository;
-import skypro.TeamWorkTelegramBot.service.restApiServices.ReportService;
-
-import java.util.ArrayList;
-import java.util.List;
-import static org.mockito.Mockito.when;
-
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-@WebMvcTest
-class ReportControllerTest {
-
-
-    @Autowired
-    private MockMvc mockMvc;
-
-    @MockBean
-    private ReportService reportService;
-
-    @MockBean
-    private ReportsRepository reportsRepository;
-
-    @MockBean
-    private ShelterController shelterController;
-
-    @MockBean
-    private VolunteerController volunteerController;
-
-    @InjectMocks
-    private ReportController reportController;
-
-
-    @SneakyThrows
-    @Test
-    public void find() {
-
-        Report report = new Report(1, "TestName", "test/path", 10L, "testType");
-
-        when(reportService.findReport(1L)).thenReturn(report);
-
-
-        mockMvc.perform(MockMvcRequestBuilders
-                .get("/animal/1")
-                        .accept(MediaType.APPLICATION_JSON))
-                        .andExpect(status().isOk());
-
-//todo неясно почему не проходит
-
-//        when(animalService.findAnimal(2L)).thenReturn(null);
+//package skypro.TeamWorkTelegramBot.controller;
+//
+//import lombok.SneakyThrows;
+//import org.json.JSONObject;
+//
+//import org.junit.jupiter.api.Test;
+//import org.mockito.InjectMocks;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+//
+//import org.springframework.boot.test.mock.mockito.MockBean;
+//
+//import org.springframework.http.MediaType;
+//
+//import org.springframework.test.web.servlet.MockMvc;
+//import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+//import skypro.TeamWorkTelegramBot.entity.Report;
+//import skypro.TeamWorkTelegramBot.repository.ReportsRepository;
+//import skypro.TeamWorkTelegramBot.service.restApiServices.ReportService;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//import static org.mockito.Mockito.when;
+//
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+//
+//@WebMvcTest
+//class ReportControllerTest {
+//
+//
+//    @Autowired
+//    private MockMvc mockMvc;
+//
+//    @MockBean
+//    private ReportService reportService;
+//
+//    @MockBean
+//    private ReportsRepository reportsRepository;
+//
+//    @MockBean
+//    private ShelterController shelterController;
+//
+//    @MockBean
+//    private VolunteerController volunteerController;
+//
+//    @InjectMocks
+//    private ReportController reportController;
+//
+//
+//    @SneakyThrows
+//    @Test
+//    public void find() {
+//
+//        Report report = new Report(1, "TestName", "test/path", 10L, "testType");
+//
+//        when(reportService.findReport(1L)).thenReturn(report);
+//
 //
 //        mockMvc.perform(MockMvcRequestBuilders
-//                        .get("/animal/2")
+//                .get("/animal/1")
 //                        .accept(MediaType.APPLICATION_JSON))
-//                .andExpect(status().is(400));
-
-    }
-
-    @SneakyThrows
-    @Test
-    void getAll() {
-        Report report1 = new Report(1, "TestName1", "test/path1", 10L, "testType");
-        Report report2 = new Report(2, "TestName2", "test/path2", 10L, "testType");
-
-        List<Report> reportList = new ArrayList<>();
-        reportList.add(report1);
-        reportList.add(report2);
-
-
-        JSONObject animalObject = new JSONObject();
-        animalObject.put("id", "2");
-        animalObject.put("name","TestName");
-        animalObject.put("filePath","test/path");
-        animalObject.put("type","testType");
-
-
-        when(reportService.getAllReports()).thenReturn(reportList);
-
-        mockMvc.perform(MockMvcRequestBuilders
-                        .get("/animal")
-                        .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
-    }
-
-
-    @SneakyThrows
-    @Test
-    void add() {
-        Report report = new Report(2, "TestName", "test/path", 10L, "testType");
-
-        JSONObject animalObject = new JSONObject();
-        animalObject.put("id", "2");
-        animalObject.put("name","TestName");
-        animalObject.put("filePath","test/path");
-        animalObject.put("type","testType");
-
-
-        when(reportService.findReport(2L)).thenReturn(report);
-        when(reportService.addAnimal(report)).thenReturn(report);
-
-        mockMvc.perform(MockMvcRequestBuilders
-                .post("/animal")
-                .content(animalObject.toString())
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
-    }
-
-    @SneakyThrows
-    @Test
-    void edit() {
-        Report report = new Report(2, "TestName", "test/path", 10L, "testType");
-
-        JSONObject animalObject = new JSONObject();
-        animalObject.put("id", "2");
-        animalObject.put("name","TestName");
-        animalObject.put("filePath","test/path");
-        animalObject.put("type","testType");
-
-
-        when(reportService.findReport(2L)).thenReturn(report);
-        when(reportService.addAnimal(report)).thenReturn(report);
-
-        mockMvc.perform(MockMvcRequestBuilders
-                        .put("/animal")
-                        .content(animalObject.toString())
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
-    }
-
-    @SneakyThrows
-    @Test
-    void remove() {
-        Report report = new Report(2, "TestName", "test/path", 10L, "testType");
-
-        JSONObject animalObject = new JSONObject();
-        animalObject.put("id", "2");
-        animalObject.put("name","TestName");
-        animalObject.put("filePath","test/path");
-        animalObject.put("type","testType");
-
-
-        when(reportService.findReport(2L)).thenReturn(report);
-        when(reportService.addAnimal(report)).thenReturn(report);
-
-        mockMvc.perform(MockMvcRequestBuilders
-                        .delete("/animal/2")
-                        .content(animalObject.toString())
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
-    }
-
-
-}
+//                        .andExpect(status().isOk());
+//
+////todo неясно почему не проходит
+//
+////        when(animalService.findAnimal(2L)).thenReturn(null);
+////
+////        mockMvc.perform(MockMvcRequestBuilders
+////                        .get("/animal/2")
+////                        .accept(MediaType.APPLICATION_JSON))
+////                .andExpect(status().is(400));
+//
+//    }
+//
+//    @SneakyThrows
+//    @Test
+//    void getAll() {
+//        Report report1 = new Report(1, "TestName1", "test/path1", 10L, "testType");
+//        Report report2 = new Report(2, "TestName2", "test/path2", 10L, "testType");
+//
+//        List<Report> reportList = new ArrayList<>();
+//        reportList.add(report1);
+//        reportList.add(report2);
+//
+//
+//        JSONObject animalObject = new JSONObject();
+//        animalObject.put("id", "2");
+//        animalObject.put("name","TestName");
+//        animalObject.put("filePath","test/path");
+//        animalObject.put("type","testType");
+//
+//
+//        when(reportService.getAllReports()).thenReturn(reportList);
+//
+//        mockMvc.perform(MockMvcRequestBuilders
+//                        .get("/animal")
+//                        .accept(MediaType.APPLICATION_JSON))
+//                .andExpect(status().isOk());
+//    }
+//
+//
+//    @SneakyThrows
+//    @Test
+//    void add() {
+//        Report report = new Report(2, "TestName", "test/path", 10L, "testType");
+//
+//        JSONObject animalObject = new JSONObject();
+//        animalObject.put("id", "2");
+//        animalObject.put("name","TestName");
+//        animalObject.put("filePath","test/path");
+//        animalObject.put("type","testType");
+//
+//
+//        when(reportService.findReport(2L)).thenReturn(report);
+//        when(reportService.addAnimal(report)).thenReturn(report);
+//
+//        mockMvc.perform(MockMvcRequestBuilders
+//                .post("/animal")
+//                .content(animalObject.toString())
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .accept(MediaType.APPLICATION_JSON))
+//                .andExpect(status().isOk());
+//    }
+//
+//    @SneakyThrows
+//    @Test
+//    void edit() {
+//        Report report = new Report(2, "TestName", "test/path", 10L, "testType");
+//
+//        JSONObject animalObject = new JSONObject();
+//        animalObject.put("id", "2");
+//        animalObject.put("name","TestName");
+//        animalObject.put("filePath","test/path");
+//        animalObject.put("type","testType");
+//
+//
+//        when(reportService.findReport(2L)).thenReturn(report);
+//        when(reportService.addAnimal(report)).thenReturn(report);
+//
+//        mockMvc.perform(MockMvcRequestBuilders
+//                        .put("/animal")
+//                        .content(animalObject.toString())
+//                        .contentType(MediaType.APPLICATION_JSON)
+//                        .accept(MediaType.APPLICATION_JSON))
+//                .andExpect(status().isOk());
+//    }
+//
+//    @SneakyThrows
+//    @Test
+//    void remove() {
+//        Report report = new Report(2, "TestName", "test/path", 10L, "testType");
+//
+//        JSONObject animalObject = new JSONObject();
+//        animalObject.put("id", "2");
+//        animalObject.put("name","TestName");
+//        animalObject.put("filePath","test/path");
+//        animalObject.put("type","testType");
+//
+//
+//        when(reportService.findReport(2L)).thenReturn(report);
+//        when(reportService.addAnimal(report)).thenReturn(report);
+//
+//        mockMvc.perform(MockMvcRequestBuilders
+//                        .delete("/animal/2")
+//                        .content(animalObject.toString())
+//                        .contentType(MediaType.APPLICATION_JSON)
+//                        .accept(MediaType.APPLICATION_JSON))
+//                .andExpect(status().isOk());
+//    }
+//
+//
+//}
--- src/test/java/skypro/TeamWorkTelegramBot/controller/ShelterControllerTest.java
+++ src/test/java/skypro/TeamWorkTelegramBot/controller/ShelterControllerTest.java
@@ -1,141 +1,141 @@
-package skypro.TeamWorkTelegramBot.controller;
-
-import lombok.SneakyThrows;
-import org.json.JSONObject;
-import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import skypro.TeamWorkTelegramBot.entity.Shelter;
-import skypro.TeamWorkTelegramBot.repository.SheltersRepository;
-import skypro.TeamWorkTelegramBot.service.restApiServices.ReportService;
-import skypro.TeamWorkTelegramBot.service.restApiServices.ShelterService;
-import skypro.TeamWorkTelegramBot.service.restApiServices.VolunteerService;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-@WebMvcTest
-class ShelterControllerTest {
-
-    @Autowired
-    private MockMvc mockMvc;
-
-    @MockBean
-    ShelterService shelterService;
-
-    @MockBean
-    SheltersRepository sheltersRepository;
-
-    @MockBean
-    ReportService reportService;
-
-    @MockBean
-    VolunteerService volunteerService;
-
-    @InjectMocks
-    private ShelterController shelterController;
-
-    @SneakyThrows
-    @Test
-    void add() {
-        Shelter shelter = new Shelter(2,"testShelter");
-
-        JSONObject shelterObject = new JSONObject();
-        shelterObject.put("id", "2");
-        shelterObject.put("name","TestName");
-
-
-        when(shelterService.findShelter(2L)).thenReturn(shelter);
-        when(shelterService.addShelter(shelter)).thenReturn(shelter);
-
-        mockMvc.perform(MockMvcRequestBuilders
-                        .post("/animal-shelter")
-                        .content(shelterObject.toString())
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
-    }
-
-    @SneakyThrows
-    @Test
-    void find() {
-        Shelter shelter = new Shelter(1,"testShelter");
-        when(shelterService.findShelter(1L)).thenReturn(shelter);
-
-        mockMvc.perform(MockMvcRequestBuilders
-                        .get("/animal-shelter/1")
-                        .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
-    }
-
-    @SneakyThrows
-    @Test
-    void getAll() {
-        Shelter shelter = new Shelter(1,"testShelter");
-        Shelter shelter2 = new Shelter(2,"testShelter2");
-
-        List<Shelter> shelterList = new ArrayList<>();
-        shelterList.add(shelter);
-        shelterList.add(shelter2);
-
-        when(shelterService.getAllShelters()).thenReturn(shelterList);
-
-        mockMvc.perform(MockMvcRequestBuilders
-                        .get("/animal-shelter")
-                        .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
-
-    }
-
-    @SneakyThrows
-    @Test
-    void edit() {
-        Shelter shelter = new Shelter(1,"testShelter");
-
-        JSONObject shelterObject = new JSONObject();
-        shelterObject.put("id", "2");
-        shelterObject.put("name","TestName");
-
-
-        when(shelterService.findShelter(2L)).thenReturn(shelter);
-        when(shelterService.addShelter(shelter)).thenReturn(shelter);
-
-        mockMvc.perform(MockMvcRequestBuilders
-                        .put("/animal-shelter")
-                        .content(shelterObject.toString())
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
-    }
-
-    @SneakyThrows
-    @Test
-    void remove() {
-        Shelter shelter = new Shelter(1,"testShelter");
-
-        JSONObject shelterObject = new JSONObject();
-        shelterObject.put("id", "2");
-        shelterObject.put("name","TestName");
-
-
-        when(shelterService.findShelter(2L)).thenReturn(shelter);
-        when(shelterService.addShelter(shelter)).thenReturn(shelter);
-
-        mockMvc.perform(MockMvcRequestBuilders
-                        .delete("/animal-shelter/1")
-                        .content(shelterObject.toString())
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
-    }
-
-
-}
+//package skypro.TeamWorkTelegramBot.controller;
+//
+//import lombok.SneakyThrows;
+//import org.json.JSONObject;
+//import org.junit.jupiter.api.Test;
+//import org.mockito.InjectMocks;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+//import org.springframework.boot.test.mock.mockito.MockBean;
+//import org.springframework.http.MediaType;
+//import org.springframework.test.web.servlet.MockMvc;
+//import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+//import skypro.TeamWorkTelegramBot.entity.Shelter;
+//import skypro.TeamWorkTelegramBot.repository.SheltersRepository;
+//import skypro.TeamWorkTelegramBot.service.restApiServices.ReportService;
+//import skypro.TeamWorkTelegramBot.service.restApiServices.ShelterService;
+//import skypro.TeamWorkTelegramBot.service.restApiServices.VolunteerService;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//import static org.mockito.Mockito.when;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+//
+//@WebMvcTest
+//class ShelterControllerTest {
+//
+//    @Autowired
+//    private MockMvc mockMvc;
+//
+//    @MockBean
+//    ShelterService shelterService;
+//
+//    @MockBean
+//    SheltersRepository sheltersRepository;
+//
+//    @MockBean
+//    ReportService reportService;
+//
+//    @MockBean
+//    VolunteerService volunteerService;
+//
+//    @InjectMocks
+//    private ShelterController shelterController;
+//
+//    @SneakyThrows
+//    @Test
+//    void add() {
+//        Shelter shelter = new Shelter(2,"testShelter");
+//
+//        JSONObject shelterObject = new JSONObject();
+//        shelterObject.put("id", "2");
+//        shelterObject.put("name","TestName");
+//
+//
+//        when(shelterService.findShelter(2L)).thenReturn(shelter);
+//        when(shelterService.addShelter(shelter)).thenReturn(shelter);
+//
+//        mockMvc.perform(MockMvcRequestBuilders
+//                        .post("/animal-shelter")
+//                        .content(shelterObject.toString())
+//                        .contentType(MediaType.APPLICATION_JSON)
+//                        .accept(MediaType.APPLICATION_JSON))
+//                .andExpect(status().isOk());
+//    }
+//
+//    @SneakyThrows
+//    @Test
+//    void find() {
+//        Shelter shelter = new Shelter(1,"testShelter");
+//        when(shelterService.findShelter(1L)).thenReturn(shelter);
+//
+//        mockMvc.perform(MockMvcRequestBuilders
+//                        .get("/animal-shelter/1")
+//                        .accept(MediaType.APPLICATION_JSON))
+//                .andExpect(status().isOk());
+//    }
+//
+//    @SneakyThrows
+//    @Test
+//    void getAll() {
+//        Shelter shelter = new Shelter(1,"testShelter");
+//        Shelter shelter2 = new Shelter(2,"testShelter2");
+//
+//        List<Shelter> shelterList = new ArrayList<>();
+//        shelterList.add(shelter);
+//        shelterList.add(shelter2);
+//
+//        when(shelterService.getAllShelters()).thenReturn(shelterList);
+//
+//        mockMvc.perform(MockMvcRequestBuilders
+//                        .get("/animal-shelter")
+//                        .accept(MediaType.APPLICATION_JSON))
+//                .andExpect(status().isOk());
+//
+//    }
+//
+//    @SneakyThrows
+//    @Test
+//    void edit() {
+//        Shelter shelter = new Shelter(1,"testShelter");
+//
+//        JSONObject shelterObject = new JSONObject();
+//        shelterObject.put("id", "2");
+//        shelterObject.put("name","TestName");
+//
+//
+//        when(shelterService.findShelter(2L)).thenReturn(shelter);
+//        when(shelterService.addShelter(shelter)).thenReturn(shelter);
+//
+//        mockMvc.perform(MockMvcRequestBuilders
+//                        .put("/animal-shelter")
+//                        .content(shelterObject.toString())
+//                        .contentType(MediaType.APPLICATION_JSON)
+//                        .accept(MediaType.APPLICATION_JSON))
+//                .andExpect(status().isOk());
+//    }
+//
+//    @SneakyThrows
+//    @Test
+//    void remove() {
+//        Shelter shelter = new Shelter(1,"testShelter");
+//
+//        JSONObject shelterObject = new JSONObject();
+//        shelterObject.put("id", "2");
+//        shelterObject.put("name","TestName");
+//
+//
+//        when(shelterService.findShelter(2L)).thenReturn(shelter);
+//        when(shelterService.addShelter(shelter)).thenReturn(shelter);
+//
+//        mockMvc.perform(MockMvcRequestBuilders
+//                        .delete("/animal-shelter/1")
+//                        .content(shelterObject.toString())
+//                        .contentType(MediaType.APPLICATION_JSON)
+//                        .accept(MediaType.APPLICATION_JSON))
+//                .andExpect(status().isOk());
+//    }
+//
+//
+//}
